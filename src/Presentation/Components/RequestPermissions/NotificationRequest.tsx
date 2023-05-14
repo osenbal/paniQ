5daaf7add8f94664bc9ca127117bfae0dd63f48c
osenbal <iqbalmaulana172703@gmail.com>
2023-05-14 09:21:23 +0700

    try notification

--- src/Presentation/Components/RequestPermissions/NotificationRequest.tsx
+++ src/Presentation/Components/RequestPermissions/NotificationRequest.tsx
@@ -14,9 +14,39 @@ const NotificationRequest = () => {
     });
   };
 
+  const sendNotif = () => {
+    if (Notification.permission === "granted") {
+      navigator.serviceWorker.getRegistration().then(function (reg: any) {
+        var options = {
+          body: "Here is a notification body!",
+          icon: "images/example.png",
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1,
+          },
+          actions: [
+            {
+              action: "explore",
+              title: "Explore this new world",
+              icon: "images/checkmark.png",
+            },
+            {
+              action: "close",
+              title: "Close notification",
+              icon: "images/xmark.png",
+            },
+          ],
+        };
+        reg.showNotification("Hello world!", options);
+      });
+    }
+  };
+
   if (!("Notification" in window)) {
     return <div>No notification in this browser</div>;
   }
+
   return (
     <>
       <div className={styles.permissionRequestor}>
@@ -27,6 +57,8 @@ const NotificationRequest = () => {
         >
           Request notification permission
         </button>
+
+        <button onClick={sendNotif}></button>
       </div>
     </>
   );
