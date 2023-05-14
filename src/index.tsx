6f0bc93f38deb24dd2b9ba5243d3491b6e5de892
osenbal <iqbalmaulana172703@gmail.com>
2023-05-14 09:13:53 +0700

    try notification

--- src/index.tsx
+++ src/index.tsx
@@ -28,8 +28,12 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
-serviceWorkerRegistration.localServiceWorkerRegister();
+
+if (process.env.NODE_ENV === "production") {
+  serviceWorkerRegistration.register();
+} else {
+  serviceWorkerRegistration.localServiceWorkerRegister();
+}
 // serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
