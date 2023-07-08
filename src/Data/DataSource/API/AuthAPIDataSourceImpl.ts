0838379f0dc546865019b013d5b1335698d8979c
osenbal <iqbalmaulana172703@gmail.com>
2023-07-09 03:34:34 +0700

    Put all endpoint to one file

--- src/Data/DataSource/API/AuthAPIDataSourceImpl.ts
+++ src/Data/DataSource/API/AuthAPIDataSourceImpl.ts
@@ -1,6 +1,8 @@
-import IAuthDataSource from "../../../Contracts/DataSource/IAuthDataSource";
+import IAuthDataSource from "@/Contracts/DataSource/IAuthDataSource";
 import axios from "@/Api/apiInterceptor";
 import { ILoginRequest } from "@/Contracts/Requests/IAuthRequest";
+import { AUTH_END_POINT } from "@/Api/LIST_END_POINT";
+import { getRefreshToken } from "@/Data/DataSource/Cookie/JWT.cookie";
 
 export default class AuthDataSourceImpl implements IAuthDataSource {
   private static instance: AuthDataSourceImpl;
@@ -15,7 +17,7 @@ export default class AuthDataSourceImpl implements IAuthDataSource {
   login<T>({ email, password }: ILoginRequest): Promise<T> {
     return axios
       .post(
-        `/auth/login`,
+        AUTH_END_POINT.POST_LOGIN,
         {
           email,
           password,
@@ -26,6 +28,22 @@ export default class AuthDataSourceImpl implements IAuthDataSource {
           },
         }
       )
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+
+  refreshToken<T>(): Promise<T> {
+    return axios
+      .get(AUTH_END_POINT.GET_REFRESH_TOKEN, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getRefreshToken()}`,
+        },
+      })
       .then((response) => {
         return response.data;
       })
