d3b5f86432710ce6068da98699098dfd6da37476
osenbal <iqbalmaulana172703@gmail.com>
2023-07-11 20:57:15 +0700

    test notif

--- src/Domain/ExternalService/FirebaseApp.ts
+++ src/Domain/ExternalService/FirebaseApp.ts
@@ -4,7 +4,7 @@ import firebaseConfig from "./config/firebase.config";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getMessaging } from "firebase/messaging";
+import { getMessaging, isSupported } from "firebase/messaging";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,7 +12,8 @@ import { getMessaging } from "firebase/messaging";
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(firebaseApp);
-const messagingApp = getMessaging(firebaseApp);
+const messagingApp = async () =>
+  (await isSupported()) && getMessaging(firebaseApp);
 
 export default firebaseApp;
 export { analytics, messagingApp };
